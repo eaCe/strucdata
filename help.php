4f2f09e11ad5befc9d035eac99f515d3b4793d23
Thorben <webmaster@slaps.de>
2017-11-22 12:25:40 +0100

    add screen

--- help.php
+++
@@ -1,3 +0,0 @@
-<p>
-    Siehe readme
-</p>
--- lib/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function getJsonLD($array)
-{
-    echo '<script type="application/ld+json">';
-    echo json_encode($array);
-    echo '</script>';   
-}
